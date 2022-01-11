0979540f5e5662a73f04ae4c160b469b1bdeb60a
Gino Siculo <ginofranco2010@live.com>
2022-01-10 22:28:26 -0300

    loader

--- src/components/Home_Components/Loader.js
+++ src/components/Home_Components/Loader.js
@@ -4,18 +4,20 @@ const Loader = ({ setLoading }) => {
   useEffect(() => {
     const timer = setTimeout(() => {
       setLoading(false)
-    }, 4000)
+    }, 3000)
     return () => clearTimeout(timer)
   })
   return (
     <div className="loader">
-      <span>L</span>
-      <span>I</span>
-      <span>T</span>
-      <span>E</span>
-      <span>N</span>
-      <span>C</span>
-      <span>I</span>
+      <p className="text">
+        <span>L</span>
+        <span>i</span>
+        <span>t</span>
+        <span>e</span>
+        <span>n</span>
+        <span>c</span>
+        <span>i</span>
+      </p>
     </div>
   )
 }
